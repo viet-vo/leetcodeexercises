b132702278cf403eb6766f053f6ac185da6b0804
Viet Vo <viet.allen.qvo@gmail.com>
2020-07-06 16:33:13 -0700

    Added some notes to IoTAll.js exercise

--- array/js/IntersectionOfTwoArraysll.js
+++ array/js/IntersectionOfTwoArraysll.js
@@ -19,12 +19,10 @@ What if the given array is already sorted? How would you optimize your algorithm
 What if nums1's size is small compared to nums2's size? Which algorithm is better?
 What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 */
-
+// ? My initial approach on this exercise is to sort these two arrays into hash maps seperately with the number as a key and iteriate the value by 1 for each instance. The sort would take take O(2n) or O(n) with a space complexity of 0(2n) or O(n). I would then loop over 1 hash map's keys to compare if they exist in the other. I would also have to check if their values are equal and push the key value many as times. (THE FOLLOWING THEORY WOULD NOT WORK) In the instance of different values, instead of trying to find the larger of the two, I can use the absolute value of their difference to discern. In the instance of different values, I would have to find the lesser value of the two and push the key the lesser value amount of times.
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersect = function(nums1, nums2) {
-    
-};
+var intersect = function (nums1, nums2) {};
