1b69168e9929061a74858825840c7a0511b930ee
Viet Vo <viet.allen.qvo@gmail.com>
2020-07-06 17:01:59 -0700

    Found a interesting 'issue when checking arrays'

--- array/js/IntersectionOfTwoArraysll.js
+++ array/js/IntersectionOfTwoArraysll.js
@@ -22,29 +22,34 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 // ? My initial approach on this exercise is to sort these two arrays into hash maps seperately with the number as a key and iteriate the value by 1 for each instance. The sort would take take O(2n) or O(n) with a space complexity of 0(2n) or O(n). I would then loop over 1 hash map's keys to compare if they exist in the other. I would also have to check if their values are equal and push the key value many as times. (THE FOLLOWING THEORY WOULD NOT WORK) In the instance of different values, instead of trying to find the larger of the two, I can use the absolute value of their difference to discern. In the instance of different values, I would have to find the lesser value of the two and push the key the lesser value amount of times.
 
 // ? For the issue of adding numbers to the return array, I believe I should should push the value as an array into the object so that I can join them easily.
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
+
 const intersect = function (nums1, nums2) {
   map1 = {};
-  for (let i = 0; i < nums1.length; i++) {
-    map1[nums1[i]] === undefined ? (map1[nums1[i]] = 1) : (map1[nums1[i]] += 1);
-  }
   map2 = {};
+  arr = [];
+  for (let i = 0; i < nums1.length; i++) {
+    map1[nums1[i]] === undefined
+      ? (map1[nums1[i]] = [nums1[i]])
+      : map1[nums1[i]].push(nums1[i]);
+  }
   for (let j = 0; j < nums2.length; j++) {
-    map2[nums2[j]] === undefined ? (map2[nums2[j]] = 1) : (map2[nums2[j]] += 1);
+    map2[nums2[j]] === undefined
+      ? (map2[nums2[j]] = [nums2[j]])
+      : map2[nums2[j]].push(nums2[j]);
   }
   console.log(map1, map2);
-  arr = [];
-  //   for (key in map1) {
-  //       if(map1[key] && map2[key]) {
-  //           arr.push()
-  //       }
-  //   }
-  arr.push("test", 2);
-  console.log(arr);
+  for (key in map1) {
+    if (map1[key] && map2[key]) {
+      arr.push();
+    }
+  }
+  console.log([2, 2] === [2, 2]);
 };
 
 intersect([1, 2, 2, 1], [2, 2]);
