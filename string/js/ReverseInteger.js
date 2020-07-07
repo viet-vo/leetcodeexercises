35a5428afe509a2bd5358de4930ad62ffa368e46
Viet Vo <viet.allen.qvo@gmail.com>
2020-07-07 04:27:43 -0700

    Quick format ReverseInteger

--- string/js/ReverseInteger.js
+++ string/js/ReverseInteger.js
@@ -2,8 +2,8 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function (x) {
-  const reversed = parseInt(x.toString().split("").reverse().join(""));
-  if (reversed > 2147483647 || reversed < -2147483647) return 0;
-  return x < 0 ? -1 * reversed : reversed;
+var reverse = function(x) {
+    const reversed = parseInt(x.toString().split('').reverse().join(''))
+    if (reversed > 2147483647 || reversed < -2147483647 ) return 0
+    return x < 0 ? -1 * reversed : reversed
 };
