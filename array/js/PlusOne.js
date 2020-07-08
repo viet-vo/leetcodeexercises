0bec469b23211e1f53df7983e9bb6df65ce1afc7
Viet Vo <viet.allen.qvo@gmail.com>
2020-07-08 05:32:48 -0700

    Finished PlusOne

--- array/js/PlusOne.js
+++ array/js/PlusOne.js
@@ -17,6 +17,8 @@ Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
 
 ? Originally, I had the idea of moving through the array with a loop and key to check if the value of the key index starting at -1 was a 9. For some reason I tried brute forcing the exercise with type manipulation. I clearly ran into the issue that would have been handled if I had gone with the former. If the integer is too large after parsing, javascript at least, incorrectly interprets the number. 
+
+? In the end, I had to work through a couple of unforseen edge cases that I had to rework the base of how I navigated the array. Taking a look at the quickest solution, you would recursively call the the function and add another parameter
 */
 
 /**
@@ -24,15 +26,21 @@ Explanation: The array represents the integer 4321.
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  console.log(seperateString(combineArray(digits) + 1 + ""));
-};
-var combineArray = function (arr) {
-  console.log(parseInt(arr.join("")));
-  return parseInt(arr.join(""));
-};
-var seperateString = function (str) {
-  return str.split("");
+    index = digits.length - 1
+    while (index >= 0) {
+        if (digits[index] === 9 && index === 0) {
+            digits[index] = 0;
+            digits.unshift(1);
+            return digits
+        } else if (digits[index] === 9) {
+            digits[index] = 0;
+        } else {
+            digits[index] += 1;
+            return digits
+        }
+        index -= 1;
+    }
 };
 
-// plusOne([1, 2, 3]);
+plusOne([9, 9]);
 plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]);
