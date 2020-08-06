4dd7cd68c151c41020e8c60588d554bf478c4839
Viet Vo <viet.allen.qvo@gmail.com>
2020-08-06 13:34:08 -0700

    Finished with my solution that should work and tested against LeetCode's solutions. The issue is the test runs do not match up with my own. Seems like the interpretation of the nums variable might be broken

--- array/js/MoveZeroes.js
+++ array/js/MoveZeroes.js
@@ -13,6 +13,7 @@ Minimize the total number of operations.
 ? Current thought process is to loop through array with a counter to keep track of length of remaining array that hasn't been check if there are 0's. Next is to remove the 0 at the point of the array I am at if there is a 0 and just add a 0 to the end.
 ? 2020.8.4 Ran into the issue of index skipping when running into a 0
 ? New approach is to count the zero then add to it after using a counter
+? There seems to be an issue with javascript's array mapping when splicing. When you splice, the actual index of the array is not updated accordingly.
 */
 /**
  * @param {number[]} nums
@@ -25,13 +26,18 @@ var moveZeroes = function (nums) {
       counter++;
     }
   }
-  nums = nums.filter(function (value, index, arr) {
-    return value !== 0;
-  });
+  for (i in nums) {
+    indexOfZero = nums.indexOf(0);
+    console.log(indexOfZero)
+    if (nums[i] === 0) {
+      nums.splice(indexOfZero, 1);
+    }
+  }
   while (counter != 0) {
     counter--;
     nums.push(0);
   }
+  console.log(nums);
 };
 
 moveZeroes([0, 1, 0, 3, 12]);
