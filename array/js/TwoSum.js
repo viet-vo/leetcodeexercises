4291fdd089f2fcaeae2fef1294ebb58cf21a2bd5
Viet Vo <viet.allen.qvo@gmail.com>
2020-08-06 21:20:58 -0700

    Initializedd and added notes for TwoSum.js

---
+++ array/js/TwoSum.js
@@ -0,0 +1,26 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+! 2020.08.06
+? Having done this one before, I know off the bat that there is a brute force method checking the sums of every possible pair of indexes. The issue with this is that the time complexity would be O(n^2).
+? The next best viable solution is to find the difference between each index of the array and then search the array for that difference.
+? To expand on the previous solution, you can first create a hashmap of the array. Then you would loop through the array only once to see if that difference exist in the hashmap at all.
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  console.log(nums + " is the list of addends for the sum: " + target);
+};
+
+twoSum([2, 7, 11, 15], 9);
