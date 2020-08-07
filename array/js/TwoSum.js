21f0481070cbfde0a3eeebd8f41dcc378a7e6b04
Viet Vo <viet.allen.qvo@gmail.com>
2020-08-06 22:38:42 -0700

    Forgot to return instead of logging

--- array/js/TwoSum.js
+++ array/js/TwoSum.js
@@ -29,7 +29,7 @@ var twoSum = function (nums, target) {
     complementDifference = target - nums[i];
     if (hash[complementDifference])
       console.log([parseInt(i), parseInt(hash[complementDifference])]);
-    return [nums[i], complementDifference];
+    return [parseInt(i), parseInt(hash[complementDifference])];
   }
   // TODO Edge case if the sum is two of the same addends (this might not need to be accounted for if it's guarenteed that the correct difference is in the array)
   // Now that the correct version of the solution is made, it certainly looks like I will run into the issue of this edge case. A solution might be to somehow look up the keys in reverse (REVERSE THE ARRAY BEFORE LOOPING) or keeping the first index in a variable and removing the first key of the duplicate value
