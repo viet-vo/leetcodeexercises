8b3e928d2ff8217101a05c155e973006ec605d50
Viet Vo <viet.allen.qvo@gmail.com>
2022-07-31 00:35:21 -0700

    Next research how to execute checks that run into erros. Possibly while loops instead.

--- tree/js/InvertBinaryTree.js
+++ tree/js/InvertBinaryTree.js
@@ -9,7 +9,7 @@
 //? First idea is that use a treeLayer counter that determines how many nodes to reverse the section of the array of.
 //? After viewing the video example in the solution video section, 2 solutions are explained both with O(n) for time and space complexity. Those 2 are recursive and interive (with fifo queue).
 //? I will work on a recursive method.
-//? Need to figure out to how to return to original root.
+//todo Try a 2 function recursion method to not run into null errors.
 
 /**
  * @param {TreeNode} root
@@ -20,20 +20,26 @@ var invertTree = function (root) {
   console.log(runs);
   runs = runs + 1;
 
-  if (root.left !== null) {
-    console.log(root);
-    root.left.val = root.left.val ^ root.right.val;
-    root.right.val = root.left.val ^ root.right.val;
-    root.left.val = root.left.val ^ root.right.val;
-    console.log(root);
-    console.log(root.root);
-    invertTree(root.left);
-  } else if (root.right !== null) {
-    console.log(root);
-    root.left.val = root.left.val ^ root.right.val;
-    root.right.val = root.left.val ^ root.right.val;
-    root.left.val = root.left.val ^ root.right.val;
-    console.log(root);
-    invertTree(root.right);
+  if ((root = null)) {
+    if (root.left !== null) {
+      console.log(root);
+      root.left.val = root.left.val ^ root.right.val;
+      root.right.val = root.left.val ^ root.right.val;
+      root.left.val = root.left.val ^ root.right.val;
+      console.log(root);
+      invertTree(root.left);
+    } else if (root.right !== null) {
+      console.log(root);
+      root.left.val = root.left.val ^ root.right.val;
+      root.right.val = root.left.val ^ root.right.val;
+      root.left.val = root.left.val ^ root.right.val;
+      console.log(root);
+      invertTree(root.right);
+    } else {
+      return;
+    }
+    return;
   }
+
+  return root;
 };
