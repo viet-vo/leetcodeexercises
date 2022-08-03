66c3bf7e555fd521a5e2ef6ef3f9e0c9185f64cd
Viet Vo <viet.allen.qvo@gmail.com>
2022-08-02 23:58:52 -0700

    TODO. Will explore solution and proccess that need to further understand next

--- tree/js/InvertBinaryTree.js
+++ tree/js/InvertBinaryTree.js
@@ -21,29 +21,25 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
- runs = 0;
- var invertTree = function (root) {
-   console.log(runs);
-   runs = runs + 1;
- 
-   if (root !== null) {
-     console.log(root);
-     // This is where we cycle through both branches. Initially, I had thought about approaching recursion this way but logically I could not follow how I would run through all the branches.
-     invertTree(root.left);
-     invertTree(root.right);
-     //todo I need to process the process of this method to fully understand. My previous todo comment would not support the method of recursion succinctly.
-     // After commenting above, following the function below would traverse the branches left-most first until null, then check for right-branch, and so on. Then, it will proceed to switch the branches.
-     // This method would make sure all the inverses are followed up from all the extremity branches proceeding with the root most branch.
-     //todo Look up why would you need to inverse a binary tree.
- 
-     // This is where we switch branches
-     temp = root.left;
-     root.left = root.right;
-     root.right = temp;
-     // I used a bitwise XOR operator method. I initially wanted to use a deconstructing assignment method to swap values but it did not resolve with the data structure of a binary tree in javascript.
-     //todo I would like to explore why deconstructing assignment would not work but bitwise XOR operator method would.
-     //! Based on testing, bitwise XOR operator method might not be a solution either.
-   }
-   return root;
- };
- 
+var invertTree = function (root) {
+  if (root !== null) {
+    // This is where we cycle through both branches. Initially, I had thought about approaching recursion this way but logically I could not follow how I would run through all the branches.
+    invertTree(root.left);
+    invertTree(root.right);
+    //todo I need to process the process of this method to fully understand. My previous todo comment would not support the method of recursion succinctly.
+    // After commenting above, following the function below would traverse the branches left-most first until null, then check for right-branch, and so on. Then, it will proceed to switch the branches.
+    // This method would make sure all the inverses are followed up from all the extremity branches proceeding with the root most branch.
+    //todo Look up why would you need to inverse a binary tree.
+
+    // This is where we switch branches
+    temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    // I used a bitwise XOR operator method. I initially wanted to use a deconstructing assignment method to swap values but it did not resolve with the data structure of a binary tree in javascript.
+    //todo I would like to explore why deconstructing assignment would not work but bitwise XOR operator method would.
+    //! Based on testing, bitwise XOR operator method might not be a solution either.
+  }
+  return root;
+};
+
+// Runtime: 92 ms, faster than 44.89% of JavaScript online submissions for Invert Binary Tree.
