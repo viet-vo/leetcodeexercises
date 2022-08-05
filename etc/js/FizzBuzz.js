1b1cbea332ca374e468d6742e211b2dce3419ea1
Viet Vo <viet.allen.qvo@gmail.com>
2022-08-04 22:20:59 -0700

    Array.forEach would not work thought process. Will reset mindset

--- etc/js/FizzBuzz.js
+++ etc/js/FizzBuzz.js
@@ -4,18 +4,13 @@
  */
 //? Should be easy solution. Have resolved FizzBuzz before.
 //? Embarressingly forgot how module works and the logic to check "if divisible by"
-let arr = [];
+
+//? After realizing the for loop needed to iterate through the whole array, Array.forEach implementation should work.
+//? Quick realization that it's a number that constructs an array is passed into function. Will reverse the changes and reapproach
+
 var fizzBuzz = function (n) {
-  for (i = 0; i !== n; i++) {
-    if (n % 3 === 0 && n % 5 === 0) {
-      arr.push("FizzBuzz" + i);
-    } else if (n % 3 === 0) {
-      arr.push("Fizz" + i);
-    } else if (n % 5 === 0) {
-      arr.push("Buzz" + i);
-    } else {
-        arr.push("" + n)
-    }
-  }
+  n.forEach((element) => {
+    console.log(element);
+  });
   return arr;
 };
