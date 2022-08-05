d5dcfc2df2ea589ba29e64ee5c2752de5ee7af68
Viet Vo <viet.allen.qvo@gmail.com>
2022-08-04 22:42:27 -0700

    Resolved FizzBuzz, understand a refreshed mindset and how that can help find solutions.

--- etc/js/FizzBuzz.js
+++ etc/js/FizzBuzz.js
@@ -7,10 +7,20 @@
 
 //? After realizing the for loop needed to iterate through the whole array, Array.forEach implementation should work.
 //? Quick realization that it's a number that constructs an array is passed into function. Will reverse the changes and reapproach
+//! Realized that n check was not checking for the correct value.
 
 var fizzBuzz = function (n) {
-  n.forEach((element) => {
-    console.log(element);
-  });
+  arr = [];
+  for (i = 0; i !== n; i++) {
+    if (n % 3 === 0 && n % 5 === 0) {
+      arr.push("FizzBuzz" + i);
+    } else if (n % 3 === 0) {
+      arr.push("Fizz" + i);
+    } else if (n % 5 === 0) {
+      arr.push("Buzz" + i);
+    } else {
+      arr.push("" + n);
+    }
+  }
   return arr;
 };
